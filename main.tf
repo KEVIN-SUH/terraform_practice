ad9e6ea63a53c970e1547850dd91423f4bb51bcc
SuhKyoonHo <tjrbsgh@naver.com>
2022-10-17 17:48:40 +0900

    main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
+locals {
+  json_data = jsondecode(file("./key.json"))
+}
+# json 파일 불러오는 형식 쓰고싶은 값을 local.json_data.## 으로 불러온다 
+# 아마 data local_file { filename=jsondecode(file("path"))}도 될것같다
 provider "aws" {
   region = "ap-northeast-2"
+  access_key = local.json_data.access_key
+  secret_key = local.json_data.secret_key
 }
 resource "aws_vpc" "practice_vpc" {
   cidr_block = "10.0.0.0/16"
