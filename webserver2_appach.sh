b4e9866e668052b19609a6eed57df729b521c124
KEVIN_cjpclass <tjrbgh@g.yju.ac.kr>
2022-10-18 19:49:35 +0900

    webserver config

---
+++ webserver2_appach.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo apt-get install python
+echo "thisi is webserver 2 welcom" | sudo tee /var/www/html/index.html
